18962d4d998b1a6bc0dc956cf8dc55ad18f6b977
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-05-15 01:01:13 -0300

    creating .eslintrc.json and eslintignore, configuring and correcting formatting

--- src/env/index.ts
+++ src/env/index.ts
@@ -8,9 +8,9 @@ const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false) {
-  console.log('Invalid environment variables:', _env.error.format());
-  throw new Error('Invalid environment variables');
+if (_env.success === false) {
+  console.log('Invalid environment variables:', _env.error.format())
+  throw new Error('Invalid environment variables')
 }
 
-export const env = _env.data;
+export const env = _env.data
