4d411e5bb60d3ee9d43e0c7448f07ea66317d69a
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-05-26 16:21:40 -0300

    first unit test

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,35 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(3)
+describe('Register Use Case', () => {
+  it('user password must be in hash format', async () => {
+    const registerUseCase = new RegisterUseCase({
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'jhondoe3@example.com',
+      password: '123456',
+    })
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
