223015a005b7b4cba59b889b3768f9ec9c53b5b8
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-06-23 17:21:21 -0300

    Token refresh strategy

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,21 @@ import { gymsRoutes } from './http/controllers/gyms/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
-
+app.register(fastifyCookie)
 app.register(usersRoutes)
 app.register(gymsRoutes)
 
--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -24,7 +24,20 @@ export async function authenticate(
 
     const token = await replay.jwtSign({}, { sign: { sub: user.id } })
 
-    return replay.status(200).send({ token })
+    const refreshToken = await replay.jwtSign(
+      {},
+      { sign: { sub: user.id, expiresIn: '7d' } },
+    )
+
+    return replay
+      .setCookie('refreshToken', refreshToken, {
+        path: '/', // route that will have access to the cookie
+        secure: true, // front-end cannot read the value
+        sameSite: true, // access only within the domain
+        httpOnly: true, // cookie will only be accessed by the backend
+      })
+      .status(200)
+      .send({ token })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return replay.status(400).send()
